a93bbe2e2229e816d6c7225211738a4bbc472efe
Derek Wing <derekwingg@gmail.com>
2022-01-13 13:56:33 -0600

    Questions request can get data from server and DB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const client = require('../database/index.js');
+const { client } = require('../database/index.js');
 
 const app = express();
 const PORT = 3000;
@@ -13,32 +13,19 @@ app.get('/', (req, res) => {
   res.send('hey from server');
 });
 
-app.get('/products', (req, res) => {
-  console.log('Client is trying to get products data...');
-  res.send('u get products data from our server');
-  const query = `SELECT * FROM products LIMIT 3`;
-  client.query(query, (err, data) => {
-    if (err) {
-      res.send('Error getting products:', err);
-      console.log('Error getting prpducts:', err);
-    } else {
-      res.send('Success getting products data from server:', data);
-      console.log('Success getting products data from server:');
-    }
-  });
-});
-
 app.get('/questions', (req, res) => {
   console.log('Client is trying to get questions data...');
-  res.send('u get questions data from our server');
   const query = `SELECT * FROM questions LIMIT 3`;
   client.query(query, (err, data) => {
     if (err) {
-      res.send('Error getting questions:', err);
-      console.log('Error getting questions:', err);
+      res.status(400).send(err);
+      console.log(
+        'Error sending back questinos data from server to client:',
+        err
+      );
     } else {
-      res.send('Success getting questions data from server:', data);
-      console.log('Success getting questions data from server:');
+      res.status(200).send(data.rows);
+      console.log('Success sending back questions data from server to client:');
     }
   });
 });
