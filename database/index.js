199d818506bd36569d384e8408868c6ff307bafa
Derek Wing <derekwingg@gmail.com>
2022-01-14 13:26:29 -0600

    Properly sends back correct format of questions data

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
-const { Client } = require('pg');
+const res = require('express/lib/response');
+const { Pool } = require('pg');
 
-const client = new Client({
+const pool = new Pool({
   host: 'localhost',
   user: 'newuser',
   password: 'password',
@@ -8,7 +9,7 @@ const client = new Client({
   port: 5432,
 });
 
-client
+pool
   .connect()
   .then((res) => {
     console.log('Connect to Postgres successfully!');
@@ -17,6 +18,39 @@ client
     console.log(err);
   });
 
+async function getQuestions(product_id) {
+  let package = {
+    product_id: product_id,
+  };
+  const query = `SELECT id, body, date_written, asker_name, reported, helpful FROM questions WHERE product_id=${product_id} LIMIT 3`;
+  let questions_data = await pool.query(query);
+  package['results'] = questions_data.rows;
+  console.log('questions_data.rows:', questions_data.rows);
+  for (let question of questions_data.rows) {
+    const question_id = question.id;
+    const query = `SELECT * FROM answers WHERE question_id=${question_id} LIMIT 3`;
+    let answers = {};
+    let answers_data = await pool.query(query);
+    for (let answer of answers_data.rows) {
+      const answer_id = answer.id;
+      const query = `SELECT * FROM answers_photos WHERE answer_id=${answer_id} LIMIT 3`;
+      const photos_data = await pool.query(query);
+      answers[answer_id] = {
+        id: answer.id,
+        body: answer.body,
+        date: answer.date_written,
+        answerer_name: answer.answerer_name,
+        helpfulness: answer.helpful,
+        photos: photos_data.rows,
+      };
+      question['answers'] = answers;
+      // console.log('photos_data.rows:', photos_data.rows);
+    }
+  }
+  return package;
+}
+
 module.exports = {
-  client,
+  pool,
+  getQuestions,
 };
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { client } = require('../database/index.js');
+const { ClientBase } = require('pg');
+const { pool, getQuestions } = require('../database/index.js');
 
 const app = express();
 const PORT = 3000;
@@ -13,36 +14,94 @@ app.get('/', (req, res) => {
   res.send('hey from server');
 });
 
-app.get('/questions', (req, res) => {
-  console.log('Client is trying to get questions data...');
-  const query = `SELECT * FROM questions LIMIT 3`;
-  client.query(query, (err, data) => {
-    if (err) {
-      res.status(400).send(err);
-      console.log(
-        'Error sending back questions data from server to client:',
-        err
-      );
-    } else {
-      res.status(200).send(data.rows);
-      console.log('Success sending back questions data from server to client:');
-    }
-  });
+app.get('/qa/questions', async (req, res) => {
+  let questions = await getQuestions(req.query.product_id);
+  res.status(400).send(questions);
+  // let package = {
+  //   product_id: req.query.product_id,
+  // };
+  // const query = `SELECT * FROM questions WHERE product_id=${req.query.product_id} LIMIT 3`;
+  // pool.query(query, async (err, data) => {
+  //   if (err) {
+  //     res.status(400).send(err);
+  //   } else {
+  //     package['results'] = data.rows;
+  //     const questions = data.rows;
+  //     let answers = [];
+  //     await questions.forEach(async (question) => {
+  //       const question_id = question.id;
+  //       const query = `SELECT * FROM answers WHERE question_id=${question_id} LIMIT 3`;
+  //       await pool.query(query, (err, data) => {
+  //         if (err) {
+  //           res.status(400).send(err);
+  //         } else {
+  //           answers.push(data.rows);
+  //           question['answers'] = data.rows;
+  //         }
+  //       });
+  //     });
+  //     for (let i = 0; i < questions.length; i++) {
+  //       questions[i]['answers'] = answers[i];
+  //     }
+  //     console.log('answers:', answers);
+  //     res.status(200).send(package);
+  //   }
+  // });
+  // const product_id = req.query.product_id;
+  // const query = `SELECT * FROM questions WHERE product_id=${product_id} LIMIT 3`;
+  // client.query(query, async (err, data) => {
+  //   if (err) {
+  //     res.status(400).send(err);
+  //   } else {
+  //     let questions = data.rows;
+  //     let answers = {};
+  //     await questions.forEach(async (question) => {
+  //       const question_id = question.id;
+  //       const query = `SELECT * FROM answers WHERE question_id=${question_id} LIMIT 3`;
+  //       await client.query(query, async (err, data) => {
+  //         if (err) {
+  //           res.status(400).send(err);
+  //         } else {
+  //           let answers = data.rows;
+  //           console.log('answers:', answers);
+  //           await answers.forEach(async (answer) => {
+  //             const answer_id = answer.id;
+  //             const query = `SELECT * FROM answers_photos WHERE answer_id=${answer_id} LIMIT 3`;
+  //             await client.query(query, async (err, data) => {
+  //               if (err) {
+  //                 res.status(400).send(err);
+  //               } else {
+  //                 answer['photos'] = data.rows;
+  //                 answers[answer_id] = answer;
+  //               }
+  //             });
+  //           });
+  //         }
+  //       });
+  //       question['answers'] = answers;
+  //     });
+  //     const queryResult = {
+  //       product_id: product_id,
+  //       results: questions,
+  //     };
+  //     res.status(200).send(queryResult);
+  //   }
+  // });
 });
 
-app.get('/answers', (req, res) => {
-  console.log('Client is trying to get answers data...');
-  const query = `SELECT * FROM answers LIMIT 3`;
-  client.query(query, (err, data) => {
-    if (err) {
-      res.status(400).send(err);
-      console.log(
-        'Error sending back answers data from server to client:',
-        err
-      );
-    } else {
-      res.status(200).send(data.rows);
-      console.log('Success sending back answers data from server to client:');
-    }
-  });
-});
+// app.get('/answers', (req, res) => {
+//   console.log('Client is trying to get answers data...');
+//   const query = `SELECT * FROM answers LIMIT 3`;
+//   client.query(query, (err, data) => {
+//     if (err) {
+//       res.status(400).send(err);
+//       console.log(
+//         'Error sending back answers data from server to client:',
+//         err
+//       );
+//     } else {
+//       res.status(200).send(data.rows);
+//       console.log('Success sending back answers data from server to client:');
+//     }
+//   });
+// });
